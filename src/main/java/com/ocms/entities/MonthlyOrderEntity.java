3a9481cf7f5c4a8581d83c86a8759b02b6748272
tanjiro2304 <103853961+tanjiro2304@users.noreply.github.com>
2024-02-21 19:32:51 +0530

    Update MonthlyOrderEntity.java

--- src/main/java/com/ocms/entities/MonthlyOrderEntity.java
+++ src/main/java/com/ocms/entities/MonthlyOrderEntity.java
@@ -1,3 +1,18 @@
+package com.ocms.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity(name = "TBL_MONTHLY_ORDER")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class MonthlyOrder{
 private Long id;
 private List<ItemEntity> items;
