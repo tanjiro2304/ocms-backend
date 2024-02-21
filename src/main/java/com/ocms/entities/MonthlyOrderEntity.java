ae93506431cd55a7e2a7c081d21596a08b1fc0b5
tanjiro2304 <103853961+tanjiro2304@users.noreply.github.com>
2024-02-21 19:29:29 +0530

    Create MonthlyOrderEntity.java

---
+++ src/main/java/com/ocms/entities/MonthlyOrderEntity.java
@@ -0,0 +1,8 @@
+public class MonthlyOrder{
+private Long id;
+private List<ItemEntity> items;
+private LocalDate startDate;
+private LocalDate endDate;
+private Double amount;
+private AccountEntity account;
+}
