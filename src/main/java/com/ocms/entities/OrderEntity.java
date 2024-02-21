99e83c82ebf9d792c9f77e6b46aa9496fa92b2c0
tanjiro2304 <103853961+tanjiro2304@users.noreply.github.com>
2024-02-21 19:31:51 +0530

    Update OrderEntity.java

--- src/main/java/com/ocms/entities/OrderEntity.java
+++ src/main/java/com/ocms/entities/OrderEntity.java
@@ -1,4 +1,18 @@
+package com.ocms.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity(name = "TBL_ORDER")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderEntity{
 private Long id;
 private List<ItemEntity> orderItems;
